349868015bfa91d0f3e897b50429751fe08d971c
Cat Lee Ball 🎶🎷🐀 <cat@catball.dev>
2022-01-22 18:17:45 -0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Cat Ball on 22/1/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
