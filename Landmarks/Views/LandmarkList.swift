c95e37453d7f645a25b3f48ab8350e3c94c36407
Cat Lee Ball 🎶🎷🐀 <cat@catball.dev>
2022-01-23 21:56:25 -0800

    Section 4: add favorites filter toggle

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -16,14 +16,15 @@ struct LandmarkList: View {
     
     var body: some View {
         NavigationView {
-            List(filteredLandmarks) {
-                landmark in
-                    NavigationLink {
-                        LandmarkDetail(landmark: landmark)
-                    } label: {
-                        LandmarkRow(landmark: landmark)
-                    }
-            }.navigationTitle("Landmarks")
+            List {
+                Toggle(isOn: $showFavoritesOnly) { Text("Show only favorites") }
+
+                ForEach(filteredLandmarks) {
+                    landmark in NavigationLink { LandmarkDetail(landmark: landmark) }
+                    label: { LandmarkRow(landmark: landmark) }
+                }
+            }
+            .navigationTitle("Landmarks")
         }
     }
 }
